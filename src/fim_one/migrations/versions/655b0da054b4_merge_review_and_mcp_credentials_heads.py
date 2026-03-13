75c291f981463feeb18f730b9a5e8da5a731834f
Tony <tony@fim.com.cn>
2026-03-13 14:58:08 +0800

    chore(migrations): merge review and mcp_credentials migration heads

---
+++ src/fim_one/migrations/versions/655b0da054b4_merge_review_and_mcp_credentials_heads.py
@@ -0,0 +1,27 @@
+"""merge review and mcp_credentials heads
+
+Revision ID: 655b0da054b4
+Revises: d5e6f7a8b901, u1v3x5z7a890
+Create Date: 2026-03-13 14:58:00.548199
+"""
+from __future__ import annotations
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '655b0da054b4'
+down_revision: Union[str, None] = ('d5e6f7a8b901', 'u1v3x5z7a890')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
